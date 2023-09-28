fcaaaaae9a59475b89b0401250a33e4bb43c9b94
Francisco Simas <franciscoamaralsimas@enta.pt>
2023-09-28 22:08:47 +0000

    Lista 3D # Vendas de combustivel # 1. Total ** # 2. Total por estação ** # 3. Total por grupo ** # 4. Qual a estação que tem mais vendas ** # 5. Qual o grupo que vendeu menos ** # 4a. Pode haver mais que uma ** ; o mesmo com os grupos **

--- Ex-3(3).py
+++ Ex-3(3).py
@@ -3,8 +3,9 @@
 # 2. Total por estação **
 # 3. Total por grupo **
 # 4. Qual a estação que tem mais vendas **
-# 5. Qual o grupo que vendeu menos
-# 4a. Pode haver mais que uma ; o mesmo com os grupos
+# 5. Qual o grupo que vendeu menos **
+# 4a. Pode haver mais que uma ** ; o mesmo com os grupos **
+from DEF import ordenar
 
 vendas = [
         # Gasolina
@@ -28,9 +29,15 @@ vendas = [
 grupos = ["Oriental", "Central", "Ocidental"]
 tipos = ["Gasolina", "Gasoleo"]
 estacoes = ["Verão", "Outono", "Inverno", "Primavera"]
-
+vendasestacao = [0] * len(estacoes)
+vendasgrupo = [0] * len(grupos)
+casa = 0
+maior = 0
 x = y = z = 0
+
+
 # Total
+
 total1 = 0
 for x in range(len(vendas)):
     for y in range(len(vendas[x])):
@@ -39,6 +46,8 @@ for x in range(len(vendas)):
             total1 += vendas[x][y][z]
 print(f"Total de vendas = {total1}")
 print()
+print("_____________________________________________________")
+print()
 
 # Total por Estação
 for y in range(len(vendas[x])):
@@ -46,14 +55,42 @@ for y in range(len(vendas[x])):
     for x in range(len(vendas)):
         for z in range(len(vendas[x][y])):
             total2 += vendas[x][y][z]
+    vendasestacao[casa] = total2
+    casa += 1
     print(f"Total de vendas no(a) {estacoes[y]} = {total2}")
 
+lista_ordenada_crescente, lista_original = ordenar(vendasestacao)
+
+maior = (ordenar(vendasestacao, -1)[0][0])
+
+print(f"O maior valor de venda por estação tem valor: {maior} na(s) estação(ões) :")
+
+for y in range(len(lista_original)):
+    if lista_original[y] == maior:
+        print(estacoes[y])
+print()
+print("_____________________________________________________")
 print()
 
 # Total por Grupo
+casa = 0
+total3 = 0
+menor = 0
 for z in range(len(vendas[x][y])):
     total3 = 0
     for x in range(len(vendas)):
         for y in range(len(vendas[x])):
             total3 += vendas[x][y][z]
+    vendasgrupo[casa] = total3
+    casa += 1
     print(f"Total de vendas no grupo {grupos[z]} = {total3}")
+
+lista_ordenada_crescente, lista_original = ordenar(vendasgrupo)
+
+menor = (ordenar(vendasgrupo)[0][0])
+
+print(f"O menor valor de venda por grupo tem valor: {menor} no(s) grupo(s) :")
+
+for z in range(len(lista_original)):
+    if lista_original[z] == menor:
+        print(grupos[z])
