1ba2d8e01b9fa9a1df63dca1b297f853ad9e4f67
ciberseguranca <goncalolopesarruda@enta.pt>
2023-10-02 10:32:27 +0000

    Strg fucnctions

---
+++ Ex-9.py
@@ -0,0 +1,9 @@
+# Strg fucnctions
+
+nome = "escola de novas tecnologias dos açores"
+print(nome.capitalize())
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+print(nome.swapcase())
+print(nome.replace("açores", "portugal"))
