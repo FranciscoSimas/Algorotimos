918838e521d4cea59beed00a271b4a88809a626d
ciberseguranca <goncalolopesarruda@enta.pt>
2023-10-02 10:31:24 +0000

    Tuples

---
+++ Ex-7.py
@@ -0,0 +1,14 @@
+# Tuples
+tuple = ([12, 10], 10, "Maria", 0o20, 0x1A)
+
+print(tuple[0])
+print(tuple[0][0])
+print(tuple[0][1])
+print(tuple[1])
+print(tuple[2])
+print(tuple[3])
+print(tuple[4])
+
+# t[1] = 20
+# print(t[1])
+# Não é possível alterar o valor de um tuple
