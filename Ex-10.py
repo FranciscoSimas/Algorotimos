fca442a35a1d9815f8f29175aede6130a8146f59
ciberseguranca <goncalolopesarruda@enta.pt>
2023-10-02 11:11:00 +0000

    Dicionário

--- Ex-10.py
+++ Ex-10.py
@@ -3,6 +3,7 @@ vendas = {}
 vendas['Terceira'] = 1
 vendas['Pico'] = 2
 vendas['São Jorge'] = 3
+# Ou vendas = {"Terceira": 1, "Pico": 2, "São Jorge": 3}
 
 print(vendas)
 
@@ -17,3 +18,16 @@ for ilha in vendas:
     vendas[ilha] = vendas[ilha] * 2
 
 print(vendas)
+
+for k in vendas.keys():
+    print(k) # Aceder ás keys
+
+for v in vendas.values():
+    print(v) # Aceder aos valores
+
+vendas.pop("São Jorge")
+print(vendas) # Retirar o valor do dicionário
+
+vendas.popitem()
+vendas.popitem()
+print(vendas) # Retirar o ultimo valor do dicionário
