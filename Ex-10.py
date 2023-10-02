d3c457a3d8b00590ebbdb877bb7ae338417082f5
ciberseguranca <goncalolopesarruda@enta.pt>
2023-10-02 11:05:47 +0000

    Dicionário

---
+++ Ex-10.py
@@ -0,0 +1,19 @@
+# [] -> lista () -> tuplo {} -> dicionário {} -> conjunto
+vendas = {}
+vendas['Terceira'] = 1
+vendas['Pico'] = 2
+vendas['São Jorge'] = 3
+
+print(vendas)
+
+ilhas = ['São Jorge', 'Faial', 'Graciosa']
+
+for ilha in ilhas:
+    vendas[ilha] = int(input(f'Vendas para {ilha}?'))
+
+print(vendas)
+
+for ilha in vendas:
+    vendas[ilha] = vendas[ilha] * 2
+
+print(vendas)
